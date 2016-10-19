14db0125b8a96959ed330dbd9be292665c4308c9
torch-light <15200223850@163.com>
2016-10-19 22:26:22 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,44 @@
+	<h1>
+	带请求的分页插件:
+	</h1>
+	<p>使用方式：
+	引入pagingPlug.js。
+	在你的angular.module('youapp',['pagingPlug'])。
+	在你需要使用该组件的html页面上面设置
+	 <pagging model="pageModel" ng-if="isPageTrue"></pagging>
+	</p>
+	<p>
+	在你的控制器第写上
+	 $scope.pageModel = {
+                model: {
+                    pageIndex: 1,
+                    type: 'payer',
+                    pageSize: 6,
+                    cardtype: -1,
+                },//传给后台的数据模型,第一个必须是页码。
+                url: 'http://xxxxxxx/MemberCard/GetMemberCardRecord',//请求的接口
+                allpage: 100,//总页码数，
+                alldata: 100,//总数据条数
+                pagenum: 6,//当前页码显示数量
+                ajaxType: 'get', //请求的类型 get post 
+                headAuth: {
+                    Authorization: 'Bearer '
+                }  后台有特殊验证的可以加在这里面,没有则为null
+
+        };
+        第一次调用后台数据时
+	 service.GetSummary(obj).then(function (data) {
+			$scope.pageModel.allpage=data.jsondata.total;  
+			$scope.pageModel.alldata=data.jsondata.totalCount;
+			//取到总的页码数,和数据条数，这样分页算出来的才是正确的，不然会是默认值
+			$scope.isPageTrue=true;
+			//设置isPageTrue为true,这个时候才会去加载分页.这个值可以自己配置
+      }，
+      在你动态改变传给后台的数据模型的时候，记得把
+      $scope.pageModel.model里面的值更新,
+      在控制器js里面写上
+        $scope.pageCall = function (obj) {
+                vm.cashiers.Summary = obj.jsondata.list;
+                //obj就是你从后台取到的值。
+         }
+	</p>
---
+++ www/js/pagingPlug.js
@@ -0,0 +1,203 @@
+  "use strict"
+        var pagging = angular.module('pagingPlug', []);
+        pagging.directive('paging', ['$http', '$q', function ($http, $q) {
+            // Runs during compile
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    model: "="
+                },
+                template: '<div class="pagging-div-warp">' +
+                  '<ul class="ul-warp">' +
+                  '<button  class="btn" ng-click="page.onePage()" ng-disabled="indexPage==1">首页</button>' +
+                  '<button  class="btn" ng-click="page.upPage()" ng-disabled="indexPage==1">上一页</button>' +
+                  '<li class="num" ng-class={true:"active",false:"notactive"}[pagging==indexPage]  ng-repeat="pagging in paggingArr track by $index" ng-click="page.index(pagging)">{{pagging}}</li>' +
+                  '<button  class="btn" ng-click="page.downPage()" ng-disabled="indexPage==model.allpage">下一页</button>' +
+                  '<button  class="btn" ng-click="page.lastPage()" ng-disabled="indexPage==model.allpage">尾页</button>' +
+                  '<span>跳页至<input ng-model="goIndex.index" ng-change="changeInputValue()"></span>' +
+                  '<button ng-click="page.sure()" class="btn" ng-if="goIndex.index">确定</button>' +
+                  '<span>共{{allpage}}页/{{alldata}}条数据</span>' +
+                  '</ul>' +
+                  '</div>',
+                link: function ($scope, element, attrs) {
+                    $scope.indexPage = 1;
+                    $scope.goIndex = {
+                        index: ''
+                    };
+                    var allpage,
+                        url,
+                        alldata,
+                        pagenum,
+                        ajaxType,
+                        headAuth = null,
+                        model = {},
+                        flag = true;
+                    var allModel = [], i = 1;
+                    if ($scope.model) {
+                        for (var _model in $scope.model) {
+                            allModel[i] = $scope.model[_model]
+                            i++;
+                        }
+                    }
+                    $scope.allpage = allModel[3] || 100;
+                    $scope.alldata = allModel[4] || 100;
+                    model = allModel[1];
+                    url = allModel[2];
+                    allpage = allModel[3] || 100;
+                    alldata = allModel[4] || 100;
+                    pagenum = allModel[5] || 6;
+                    ajaxType = allModel[6] || 'get';
+                    headAuth = allModel[7] || null;
+                    var pageMid = Math.floor(pagenum / 2);
+                    var flipnum;
+                    $scope.apiCall = {
+                        mosaic: function (param) {
+                            var u = "?";
+                            for (var x in param) {
+                                u = u + x + '=' + param[x] + '&';
+                            }
+                            u = u.substr(0, u.length - 1);
+                            return u;
+                        },
+                        getRequest: function () {
+                            flag = true;
+                            for (var _model in model) {
+                                if (flag) {
+                                    model[_model] = $scope.indexPage;
+                                    flag = false;
+                                }
+                            }
+                            var result = [];
+                            result.$promise = null;
+                            ajaxType = ajaxType.toLowerCase();
+                            switch (ajaxType) {
+                                case "get":
+                                    result.$promise = $http.get(url + $scope.apiCall.mosaic(model));
+                                    break;
+                                case "post":
+                                    result.$promise = $http.post(url, model);
+                                    break;
+                            }
+                            return result.$promise;
+                        },
+                        addToken: function () {
+                            if (allModel.length > 0) {
+                                if (headAuth) {
+                                    $http.defaults.headers.common = headAuth;
+                                };
+                                $scope.apiCall.getRequest().success(function (data) {
+                                    try{
+                                       $scope.$parent.call(data);
+                                     }catch(err){
+                                      console.log(err+'请在父级Scope定义$scope.call()函数');
+                                     }
+                                   
+                                })
+                            }
+
+                        }
+                    };
+                    $scope.page = {
+                        init: function () {
+                            $scope.paggingArr = [];
+                            if (allpage < pagenum) {
+                                pagenum = allpage;
+                            };
+                            for (var i = 1; i <= pagenum; i++) {
+                                $scope.paggingArr[i - 1] = i;
+                            };
+                        },
+                        createPage: function () {
+                            if (allpage > pagenum) {
+                                var temp = $scope.paggingArr[0] + flipnum;
+                                if (temp < 1) {
+                                    $scope.page.init();
+                                } else {
+                                    for (var i = 1; i <= pagenum; i++) {
+                                        $scope.paggingArr[i - 1] = $scope.paggingArr[i - 1] + flipnum;
+                                    }
+                                }
+                            }
+                        },
+                        index: function (n) {
+                            flipnum = n - $scope.paggingArr[pageMid];
+                            if (n + flipnum > allpage) {
+                                flipnum = allpage - n;
+                            };
+                            $scope.indexPage = n;
+                            if (flipnum > 0 && $scope.paggingArr[pagenum - 1] < allpage) {
+                                $scope.page.createPage();
+                            } else if (flipnum < 0 && $scope.paggingArr[0] > 1) {
+                                $scope.page.createPage();
+                            }
+
+                            $scope.apiCall.addToken();
+
+                        },
+                        onePage: function () {
+                            if ($scope.indexPage > 1) {
+                                $scope.indexPage = 1;
+                                $scope.apiCall.addToken();
+                                $scope.page.init();
+                            }
+
+                        },
+                        upPage: function () {
+                            $scope.indexPage -= 1;
+                            if ($scope.indexPage < 1) {
+                                $scope.indexPage = 1;
+                            }
+                            $scope.page.index($scope.indexPage);
+
+                        },
+                        downPage: function () {
+                            $scope.indexPage += 1;
+                            if ($scope.indexPage > allpage) {
+                                $scope.indexPage = allpage;
+                            }
+                            $scope.page.index($scope.indexPage);
+                        },
+                        lastPage: function () {
+                            if ($scope.indexPage < allpage) {
+                                $scope.indexPage = allpage;
+                                $scope.apiCall.addToken();
+                                for (var i = 1; i <= pagenum; i++) {
+                                    $scope.paggingArr[i - 1] = allpage - pagenum + i;
+                                };
+                            }
+                        },
+                        sure: function () {
+                            $scope.goIndex.index = $scope.goIndex.index - 0;
+                            $scope.indexPage = $scope.goIndex.index;
+                            if ($scope.indexPage + pageMid <= allpage) {
+                                for (var i = 1; i <= pagenum; i++) {
+                                    $scope.paggingArr[i - 1] = $scope.indexPage + i - pageMid;
+                                    if ($scope.paggingArr[i - 1] <= 0) {
+                                        $scope.page.init();
+                                        break;
+                                    }
+                                }
+                            } else if ($scope.indexPage + pageMid > allpage) {
+                                for (var i = 1; i <= pagenum; i++) {
+                                    $scope.paggingArr[i - 1] = allpage - pagenum + i;
+                                };
+                            }
+                            $scope.apiCall.addToken();
+                            $scope.goIndex.index = '';
+                        }
+                    };
+                    $scope.changeInputValue = function () {
+                        if ($scope.goIndex.index > allpage) {
+                            $scope.goIndex.index = allpage;
+                        }
+                        if ($scope.goIndex.index < 1) {
+                            $scope.goIndex.index = 1;
+                        }
+                    }
+
+                    $scope.page.init();
+                }
+
+            };
+        }]);
